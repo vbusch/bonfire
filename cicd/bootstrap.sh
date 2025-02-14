edaba0ceecbfbc01529d99e1efd2c0c43e0ca13e
Vanessa Busch <vbusch@redhat.com>
2025-02-14 11:57:43 -0500

    chore: debugging ci docker issue (DO NOT MERGE)
    Signed-off-by: Vanessa Busch <vbusch@redhat.com>

--- cicd/bootstrap.sh
+++ cicd/bootstrap.sh
@@ -2,7 +2,7 @@
 
 # which branch to fetch cicd scripts from in cicd-tools repo
 CICD_REPO_BRANCH="${CICD_REPO_BRANCH:-main}"
-CICD_REPO_ORG="${CICD_REPO_ORG:-RedHatInsights}"
+CICD_REPO_ORG="${CICD_REPO_ORG:-vbusch}"
 BOOTSTRAP_SCRIPT_URL="https://raw.githubusercontent.com/${CICD_REPO_ORG}/cicd-tools/${CICD_REPO_BRANCH}/bootstrap.sh"
 
 set -e
